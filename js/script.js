de4ca845b2432a278b8cdfa972afd08a7dc3a255
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-24 15:27:59 +0100

    Array-definition

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Definizione Array
+const emailList = [
+    "luigi098@gmail.com"
+    "lorenzo088@gmail.com"
+    "luigia098@gmail.com"
+    "castelluccilu98@gmail.com"
+    "morgana@gmail.com"
+    "leopoldi@gmail.com"
+    "roberto@gmail.com"
+    "bolean234@gmail.com"
+    "booleano@gmail.com"
+    "master984@gmail.com"
+    "oro@gmail.com"
+    "platino@gmail.com"
+    "diamatti@gmail.com"
+    "orione@gmail.com"
+];
